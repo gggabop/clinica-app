2f1a782d118356cce5e0f0cd76d2ddd1465ac822
Gabriel Pérez <jgperezp03@gmail.com>
2024-03-29 00:06:55 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,25 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import Vuetify from './vuetify'; // Adjust the path if necessary
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(Vuetify) // Use Vuetify here
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
